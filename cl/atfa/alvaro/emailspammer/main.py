9b1666a455cec4ab9fb3089c8e8c91b643b489ff
Álvaro Lucas Castillo Calabacero <alvarolucascc96@gmail.com>
2020-04-30 18:50:47 -0400

    feat(main): implements a basic email-senderbot

--- cl/atfa/alvaro/emailspammer/main.py
+++ cl/atfa/alvaro/emailspammer/main.py
@@ -3,11 +3,38 @@
 #   contact alvaro.castillo@alumnos.ucn.cl
 #   Do not use in production.
 
-# @author Alvaro Castillo Calabacero
+# SMTP and SSL modules
+import smtplib
+import ssl
 
-import socket
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("www.python.org", 80))
-#print(s)
+smtpServer = "smtp.gmail.com"
+port = 587  # For starttls
+
+# user and password for sender mail
+fromEmail = "tstspamm@gmail.com"
+password = input("Type your password and press enter: ")
+
+# Victim email test
+toEmail = "pcastanedarivera.940807@gmail.com"
+
+# Message
+msg = """
+ Test message from tstspamm@gmail.com
+"""
+# SSL context
+sslContext = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtpServer, port)
+    server.starttls(context=sslContext) # Secure the connection
+    server.login(fromEmail, password)
+
+    for i in range(10):
+        server.sendmail(fromEmail, toEmail, msg)
+
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
